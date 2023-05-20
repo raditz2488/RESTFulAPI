f95bc0a882c25a20fb3cf87d58daed250c69529e
robh <rohan.bhale2411@gmail.com>
2023-05-20 20:25:44 +0530

    Parsed path from request.

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 
 // Dependency
 var http = require('http');
+var url = require('url');
 
 // Respond to all requests with a string
 var server = http.createServer(function(req, res){
-    res.end('Hellow world')
+    // Parse url
+    var parsedUrl = url.parse(req.url, true)
+
+    // Get path
+    var path = parsedUrl.path
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send response 
+    res.end('Hellow world');
+
+    // Print path
+    console.log('The path from request is: '+ trimmedPath);
 });
 
 // Listen on port 3000
