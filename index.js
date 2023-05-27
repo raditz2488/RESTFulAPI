49f287bf59754980f432af981cecb4327c126a77
robh <rohan.bhale2411@gmail.com>
2023-05-27 16:32:02 +0530

    Extracts payload from request.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Dependency
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // Respond to all requests with a string
 var server = http.createServer(function(req, res){
@@ -22,11 +23,27 @@ var server = http.createServer(function(req, res){
     // Get headers
     var headers = req.headers;
 
-    // Send response 
-    res.end('Hellow world');
+    // Collect the payload if it exists
+    // Create the string decoder 
+    var decoder = new StringDecoder('utf-8');
 
-    // Print request data to console
-    console.log('The headers in the request are \n', headers);
+    // Collect payload string asynchronously
+    var payload = ''
+    req.on('data', function(data) {
+        payload += decoder.write(data);
+    });
+
+    // When request completes end the collection of payload
+    // Send the response and log payload
+    req.on('end', function() {
+        payload += decoder.end();
+
+        // Send response 
+        res.end('Hellow world');
+
+        // Print request data to console
+        console.log('The payload in the request is: ', payload);
+    });
 });
 
 // Listen on port 3000
